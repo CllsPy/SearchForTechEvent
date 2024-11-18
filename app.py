88369f4d705d918eb87d4862dc4f0b8b57e84b25
CLL <96326019+CllsPy@users.noreply.github.com>
2024-11-18 20:18:26 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from scripts import get_readme
+
+st.set_page_config(
+    page_title='New-App',
+    layout='centered',
+    
+)
+
+st.sidebar.title('Sidebar')
+st.title('New-App')
+url = st.text_input('URL')
+
+st.write(get_readme.get_readme_file(url))
+
+
+
---
+++ scripts/get_readme.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import wget
+
+PATH = 'scripts'
+
+def get_readme_file(url: str) -> str:
+    """get readme from github repo"""
+
+    readfile = wget.download(url, PATH)
+    f = open(readfile)
+    r = f.read()
+
+    return r
